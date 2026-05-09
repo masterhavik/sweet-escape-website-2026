d692bf345415ea54a44b7e901ddc6025563c4bb5
masterhavik <kevenmc@gmail.com>
2026-05-08 20:55:10 -0400

    Use PMS platform colors on property detail listing button

--- src/pages/PropertyDetailPage.tsx
+++ src/pages/PropertyDetailPage.tsx
@@ -1,5 +1,6 @@
 import { Link, useParams } from 'react-router-dom';
 import { properties } from '../data/properties';
+import { getPlatformButtonClass } from '../data/platformPalette';
 
 export function PropertyDetailPage() {
   const { propertySlug } = useParams();
@@ -29,22 +30,19 @@ export function PropertyDetailPage() {
           <p className="lead">{property.location}</p>
           <p>{property.description}</p>
           <div className="button-row">
-            <Link className="button button--primary" to="/book-now">
-              Book Direct
+            <Link className="button button--primary" to="/rentals">
+              Back to Rentals
             </Link>
-            <a className="button button--secondary" href={property.bookingUrl} target="_blank" rel="noreferrer">
-              View Current {property.externalBookingLabel} Listing
+            <a
+              className={`button ${getPlatformButtonClass(property.externalBookingLabel)}`}
+              href={property.bookingUrl}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {property.externalBookingLabel} Listing
             </a>
           </div>
         </div>
-      </section>
-
-      <section className="panel">
-        <h2>Booking widget placeholder</h2>
-        <p>
-          This is where a future direct-booking widget, availability tool, or booking engine embed
-          will be placed once the site build and provider choice are finalized.
-        </p>
       </section>
     </div>
   );
